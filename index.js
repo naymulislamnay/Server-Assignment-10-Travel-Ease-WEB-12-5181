0f1240d6aba767020852bb490fad5b7ab5366018
naymulislamnay <naymulislamnay@gmail.com>
2025-11-12 23:32:19 +0600

    basic server done

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@travelease.vacwmlf.mongodb.net/?appName=TravelEase`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('server is running fine---------')
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
