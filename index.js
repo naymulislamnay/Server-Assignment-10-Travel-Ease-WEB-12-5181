678d762c3c51d1a37fb73ad9dda9ea5555e52aad
naymulislamnay <naymulislamnay@gmail.com>
2025-11-19 22:56:20 +0600

    successfully connected to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +9,32 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@travelease.vacwmlf.mongodb.net/?appName=TravelEase`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('This is my last try')
 })
+
+async function run() {
+    try {
+        await client.connect()
+        await client.db('admin').command({ ping: 1 });
+        console.log("Pinged Your Deployment. You Successfully Connected to MongoDB");
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`This server is running on port: ${port}`)
