edb045600ae192fa8131edf7852ff3f2058ba9fe
naymulislamnay <naymulislamnay@gmail.com>
2025-11-19 23:22:34 +0600

    vehicle details API created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -54,7 +54,13 @@ async function run() {
             res.send(result);
         })
 
-
+        // Vehicle Details API
+        app.get('/vehicles/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await vehiclesCollection.findOne(query);
+            res.send(result);
+        })
 
 
         await client.db('admin').command({ ping: 1 });
